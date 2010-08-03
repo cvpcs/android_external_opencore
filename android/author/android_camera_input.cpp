82f0798b3bbd89c43588610b99579d839bf6f01b
cvpcs <root@cvpcs.org>
2010-08-03 16:03:37 -0500

    ignore set prop errors [cyanogen]

--- android/author/android_camera_input.cpp
+++ android/author/android_camera_input.cpp
@@ -1111,7 +1111,7 @@ PVMFStatus AndroidCameraInput::DoInit()
     if (mCamera->setParameters(s) != NO_ERROR)
     {
         LOGE("Failed to set camera(%p) parameters", mCamera.get());
-        return PVMFFailure;
+        //return PVMFFailure;
     }
 
     // Since we may not honor the preview size that app has requested
